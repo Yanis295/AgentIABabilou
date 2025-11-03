9d44f7b1210acc6977b5e36bee6aad1748e1a7f2
Yanis295 <ypires@fiveforty.fr>
2025-11-03 16:36:03 +0100

    Update API endpoint for Power Platform

--- d365-sso-webapp/public/js/copilot-authenticated.js
+++ d365-sso-webapp/public/js/copilot-authenticated.js
@@ -11,7 +11,8 @@ class CopilotAuthenticated {
         this.config = {
             environmentId: '308d8cf6-baa8-eba2-8a15-8afc891fddf9',
             botId: 'cr288_chatbotAgentIaBabilou',
-            apiEndpoint: 'https://org2b2efe61.crm12.dynamics.com',
+            // L'endpoint API Power Platform (pas Dataverse)
+            apiEndpoint: 'https://308d8cf6baa8eba28a158afc891fdd.f9.environment.api.powerplatform.com',
             apiVersion: '2022-03-01-preview',
             tokenEndpoint: '/api/get-powerplatform-token'
         };
